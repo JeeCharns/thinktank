e35f960435e6d1db62819228def89cd8306c37d1
Ella Fenton <ellaraefenton@gmail.com>
2022-05-07 19:37:45 -0600

    feat: adds some detail / context for firebase!

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -9,10 +9,14 @@ const Home = () => {
   const [documentId, setDocumentId] = useState(null);
 
   const handleCreateThinkTank = async () => {
+    // if the question length exists, we don't create a document
+    // and may want to show an error or disable the button
     if (!question.length) {
       console.log("no question!");
       return;
     }
+    // otherwise, create a new document in the ThinkTanks collection
+    // then set the document id to state so we can identify the ThinkTank
     await db
       .collection("ThinkTanks")
       .add({
--- src/pages/other-page.jsx
+++ src/pages/other-page.jsx
@@ -3,9 +3,9 @@ import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { db } from "../firebase/firebase";
 
+// A helper function to get the query params in a url
 const useQuery = () => {
   const { search } = useLocation();
-
   return React.useMemo(() => new URLSearchParams(search), [search]);
 };
 
@@ -18,15 +18,21 @@ const OtherPage = () => {
   const query = useQuery();
 
   useEffect(() => {
+    // when the page loads...
+    // we get the query params in the url so in "https://localhost/3000?id=1234"
+    // the query params are id=1234
     const docId = query.get("id");
 
     const loadThinkTank = async () => {
+      // we got to firebase and look for this documentId
       const docRef = db.collection("ThinkTanks").doc(docId);
       const doc = await docRef.get({ source: "server" });
       if (!doc.exists) {
+        // if it doesn't exist, we need to handle this error
         console.log("ooops - no document exists!!");
       } else {
-        console.log('success!!! found the document')
+        // if it does, we create a snapshot of the document and set it to state
+        console.log("success!!! found the document");
         return docRef.onSnapshot((doc) => {
           setThinktankDocRef(docRef);
           setThinktankData(doc.data());
